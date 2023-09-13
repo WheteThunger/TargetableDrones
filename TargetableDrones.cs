ace69bb181c020f655c1800b48ef5d34fc631da3
WhiteThunder <whetethunger@gmail.com>
2023-09-12 20:23:01 -0700

    Allow skinned drones to be targeted

--- TargetableDrones.cs
+++ TargetableDrones.cs
@@ -13,7 +13,7 @@ using HumanNpc = global::HumanNPC;
 
 namespace Oxide.Plugins
 {
-    [Info("Targetable Drones", "WhiteThunder", "1.2.1")]
+    [Info("Targetable Drones", "WhiteThunder", "1.2.2")]
     [Description("Allows RC drones to be targeted by Auto Turrets and SAM Sites.")]
     internal class TargetableDrones : CovalencePlugin
     {
@@ -333,7 +333,7 @@ namespace Oxide.Plugins
 
         private static bool IsDroneEligible(Drone drone)
         {
-            return drone.skinID == 0 && !(drone is DeliveryDrone);
+            return !(drone is DeliveryDrone);
         }
 
         private static Drone GetParentDrone(BaseEntity entity)
