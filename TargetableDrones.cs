d53a87207b04db836468da4eac6184f0b679f09a
WhiteThunder <whetethunger@gmail.com>
2024-03-26 21:32:07 -0700

    Increment patch version

--- TargetableDrones.cs
+++ TargetableDrones.cs
@@ -13,7 +13,7 @@ using HumanNpc = global::HumanNPC;
 
 namespace Oxide.Plugins
 {
-    [Info("Targetable Drones", "WhiteThunder", "1.2.3")]
+    [Info("Targetable Drones", "WhiteThunder", "1.2.4")]
     [Description("Allows RC drones to be targeted by Auto Turrets and SAM Sites.")]
     internal class TargetableDrones : CovalencePlugin
     {
