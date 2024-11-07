956f072eb89e285a5efa40dc0ca2a2deebf4898e
WhiteThunder <whetethunger@gmail.com>
2024-11-06 22:08:04 -0800

    Fix for Rust update

--- TargetableDrones.cs
+++ TargetableDrones.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Oxide.Core;
-using Rust.AI;
+using Rust.Ai;
 using UnityEngine;
 using VLB;
 using static SamSite;
@@ -13,7 +13,7 @@ using HumanNpc = global::HumanNPC;
 
 namespace Oxide.Plugins
 {
-    [Info("Targetable Drones", "WhiteThunder", "1.2.5")]
+    [Info("Targetable Drones", "WhiteThunder", "1.2.6")]
     [Description("Allows RC drones to be targeted by Auto Turrets and SAM Sites.")]
     internal class TargetableDrones : CovalencePlugin
     {
